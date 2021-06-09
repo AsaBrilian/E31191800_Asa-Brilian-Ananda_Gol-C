eeee82572678773b69901df64c648104452209ae
AsaBrilian <66717938+AsaBrilian@users.noreply.github.com>
2021-06-09 22:55:33 +0700

    tugas android

---
+++ crud_volley/app/src/main/java/com/example/crud_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.crud_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.56.1/db_ci/android/API";
+    public static final String URL_INSERT = "http://192.168.56.1/db_ci/android/APIinsert";
+    public static final String URL_DELETE = "http://192.168.56.1/db_ci/android/APIdelete";
+    public static final String URL_UPDATE = "http://192.168.56.1/db_ci/android/APIupdate";
+}
